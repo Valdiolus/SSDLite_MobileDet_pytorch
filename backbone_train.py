394bb192e4d547a2272615d8a5c449b5fcf0ea8e
Valdis Gerasymiak <valdis@MacBook-Pro-Valdis.local>
2022-11-06 18:03:21 +0200

    added resume

--- backbone_train.py
+++ backbone_train.py
@@ -18,8 +18,8 @@ import wandb
 
 # CUDA_VISIBLE_DEVICES="" for CPU run
 
-imagenet_dir = '/Users/valdis/datasets/imagenet/ILSVRC/Data/CLS-LOC'
-batch_size = 64
+#imagenet_dir = '/Users/valdis/datasets/imagenet/ILSVRC/Data/CLS-LOC'
+batch_size = 32
 workers = 4
 EPOCHS = 150
 n_classes = 1000
@@ -27,6 +27,12 @@ input_size = 320
 init_lr = 0.05
 init_momentum = 0.9
 init_weight_decay = 0.00004
+CenterCrop = 320 # 224? was 298 
+
+load_from_file = 0
+saved_model = '2022-11-6_18-30-23'
+wandb_saved_id = 0
+saved_epochs = 0
 
 PATH_TO_SAVE = './runs'
 
@@ -41,17 +47,22 @@ wandb_config = {"batch_size": batch_size,
                 "lr": init_lr,
                 "momentum": init_momentum,
                 "weight_decay": init_weight_decay,
+                "CenterCrop": CenterCrop,
                 }
 
 
 def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):  
-    #count the current date and time
-    time_struct = time.gmtime()
-    time_now = str(time_struct.tm_year)+'-'+str(time_struct.tm_mon)+'-'+str(time_struct.tm_mday)+'_'+str(time_struct.tm_hour+3)+'-'+str(time_struct.tm_min)+'-'+str(time_struct.tm_sec)
-    print(time_now)
-    train_path = os.path.join(PATH_TO_SAVE, time_now)
-    if not os.path.exists(train_path):
-        os.makedirs(train_path)
+    if load_from_file:
+        print("Resume training:", saved_model)
+        train_path = os.path.join(PATH_TO_SAVE, saved_model)
+    else:
+        #count the current date and time
+        time_struct = time.gmtime()
+        time_now = str(time_struct.tm_year)+'-'+str(time_struct.tm_mon)+'-'+str(time_struct.tm_mday)+'_'+str(time_struct.tm_hour+3)+'-'+str(time_struct.tm_min)+'-'+str(time_struct.tm_sec)
+        print("New train:", time_now)
+        train_path = os.path.join(PATH_TO_SAVE, time_now)
+        if not os.path.exists(train_path):
+            os.makedirs(train_path)
     
     model.to(device)  
     #print(model)
@@ -59,10 +70,34 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
     best_acc = 0.0
 
     #wand integration
-    wandb.init(project="Mobiledet backbone", name = time_now, config=wandb_config) # resume
+    if load_from_file:
+        #restore wandb
+        with open(os.path.join(train_path, "wandb.txt"), 'r') as file1:
+            wandb_saved_id = file1.read()
+
+        wandb.init(id=wandb_saved_id, resume=True, project="Mobiledet backbone", name = saved_model, config=wandb_config)
+
+        #restore epochs number
+        with open(os.path.join(train_path, "hyp.txt"), 'r') as file2:
+            saved_epochs = int(file2.read())
+    else:
+        wandb_saved_id = wandb.util.generate_id()
+        wandb.init(id=wandb_saved_id, resume="allow", project="Mobiledet backbone", name = time_now, config=wandb_config)
+        
+        #save id in the same folder
+        with open(os.path.join(train_path, "wandb.txt"), 'w') as f:
+            f.write(wandb_saved_id)
+            f.close()
+        
+        with open(os.path.join(train_path, "hyp.txt"), 'w') as f:
+            f.write(0)
+            f.close()
+        
+    print("wandb id:", wandb_saved_id)
+
     if device == torch.device("cuda"):
         print("Use wandb watch to collect model info")
-        wandb.watch(model, log_freq=wandb_log_interval)
+        #wandb.watch(model, log_freq=wandb_log_interval)
 
     time_beginning = time.time()
     losses = {'train': [], "val": []}
@@ -70,7 +105,8 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
     #log_template = "\nEpoch {ep:03d} train_acc {t_acc:0.4f} val_acc {v_acc:0.4f} train_loss: {t_loss:0.4f} val_loss {v_loss:0.4f}"
 
     #pbar = trange(num_epochs, desc="Epoch")
-    for epoch in range(num_epochs):
+    for epoch in range(saved_epochs, num_epochs):
+
         for phase in ["train", "val"]:
             if phase == 'train':
                 model.train(True)
@@ -121,11 +157,12 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
                         running_accuracy=(running_corrects/(batch_size*iter))
 
                         if iter % wandb_log_interval == 0:
-                            wandb.log({"train": {"loss": loss.item(), "accuracy": running_accuracy.item()}})
+                            wandb.log({"train": {"loss": loss.item(), "accuracy": running_accuracy.item(), "lr": scheduler.get_last_lr()[0]}})
 
-                        #if iter % 10 == 0:
-                        #    torch.save(model.state_dict(), os.path.join(PATH_TO_SAVE, "tmp.pt"))
-                        #    exit(0)
+                        #if iter % 100 == 0:
+                        #    #torch.save(model.state_dict(), os.path.join(PATH_TO_SAVE, "tmp.pt"))
+                        #    torch.save(model.state_dict(), os.path.join(train_path, "last.pt"))
+
                         tepoch.set_postfix(loss=loss.item(), accuracy=running_accuracy.item())
                     if phase == "val":
                         iter_top1 = 0
@@ -155,9 +192,12 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
 
             if phase == 'train':
                 scheduler.step()
-                #wandb.log({"train epoch loss": epoch_loss, "train epoch accuracy": epoch_acc})
+                wandb.log({"train": {"epoch loss": epoch_loss, "epoch accuracy": epoch_acc}})
                 
             if phase == 'val':
+                with open(os.path.join(train_path, "hyp.txt"), 'w') as f:
+                    f.write(epoch+1)
+                    f.close()
                 correct_top1 = correct_top1.item() / processed_data
                 correct_top5 = correct_top5.item() / processed_data
                 torch.save(model.state_dict(), os.path.join(train_path, "last.pt"))
@@ -181,19 +221,28 @@ if __name__ == '__main__':
         print('CUDA is available!  Training on GPU ...')
         train_on_gpu = True
         device = torch.device("cuda")
+        with open("cuda_datapath.txt", 'r') as f:
+            imagenet_dir = f.read()
+            print("cuda imagenet path:", imagenet_dir)
     else:
         if torch.backends.mps.is_available():
             print('GPU on M1 MAC is available!  Training on GPU ...')
             train_on_gpu = True
             device = torch.device("mps")
+            with open("mps_datapath.txt", 'r') as f:
+                imagenet_dir = f.read()
+                print("mps imagenet path:", imagenet_dir)
         else:
             print('CUDA and MPS are not available.  Training on CPU ...')
+            with open("cuda_datapath.txt", 'r') as f:
+                imagenet_dir = f.read()
+                print("cuda imagenet path:", imagenet_dir)
 
 
     #Loading dataset - like Imagenet, where 1k folders with each classes
     data_transforms = transforms.Compose([
         transforms.Resize((input_size,input_size)),
-        transforms.CenterCrop(298),
+        transforms.CenterCrop(CenterCrop),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
@@ -210,6 +259,10 @@ if __name__ == '__main__':
     #Configurate model and hyperparameters
 
     model = mobiledet.MobileDetTPU(net_type="classifier", classes=n_classes)
+
+    if load_from_file:
+        #model.load_state_dict(saved_model)
+        model.load_state_dict(torch.load(os.path.join(os.path.join(PATH_TO_SAVE, saved_model), "last.pt"))) #./wandb/run-20221106_155148-2s2e31rf
 
     #model = torchvision.models.mobilenet_v2(weights=torchvision.models.MobileNet_V2_Weights.IMAGENET1K_V2)
     #model.classifier = nn.Linear(1280, n_classes)
