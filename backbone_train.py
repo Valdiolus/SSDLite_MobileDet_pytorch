6d6baf3eb0c1d86253fed1138b0164ecdcfe2e20
Valdis Gerasymiak <valdis@MacBook-Pro-Valdis.local>
2022-11-07 10:44:11 +0200

    added additional restore functions

--- backbone_train.py
+++ backbone_train.py
@@ -16,7 +16,12 @@ import mobiledet
 from torchsummary import summary
 import wandb
 
+import argparse
+
 # CUDA_VISIBLE_DEVICES="" for CPU run
+
+#for time correction
+gmt_dst = 2
 
 #imagenet_dir = '/Users/valdis/datasets/imagenet/ILSVRC/Data/CLS-LOC'
 batch_size = 32
@@ -30,7 +35,7 @@ init_weight_decay = 0.00004
 CenterCrop = 320 # 224? was 298 
 
 load_from_file = 0
-saved_model = '2022-11-6_21-50-46'
+saved_model = '2022-11-7_8-59-55'
 wandb_saved_id = 0
 
 PATH_TO_SAVE = './runs'
@@ -50,14 +55,14 @@ wandb_config = {"batch_size": batch_size,
                 }
 
 
-def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):  
+def train(model, dataloaders, loss_fn, optimizer, scheduler, iter_per_epoch, num_epochs = 10):  
     if load_from_file:
         print("Resume training:", saved_model)
         train_path = os.path.join(PATH_TO_SAVE, saved_model)
     else:
         #count the current date and time
         time_struct = time.gmtime()
-        time_now = str(time_struct.tm_year)+'-'+str(time_struct.tm_mon)+'-'+str(time_struct.tm_mday)+'_'+str(time_struct.tm_hour+3)+'-'+str(time_struct.tm_min)+'-'+str(time_struct.tm_sec)
+        time_now = str(time_struct.tm_year)+'-'+str(time_struct.tm_mon)+'-'+str(time_struct.tm_mday)+'_'+str(time_struct.tm_hour+gmt_dst)+'-'+str(time_struct.tm_min)+'-'+str(time_struct.tm_sec)
         print("New train:", time_now)
         train_path = os.path.join(PATH_TO_SAVE, time_now)
         if not os.path.exists(train_path):
@@ -69,7 +74,7 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
     best_acc = 0.0
     saved_epochs = 0
 
-    #wand integration
+    #restore wandb, epochs count and lr scheduler
     if load_from_file:
         #restore wandb
         with open(os.path.join(train_path, "wandb.txt"), 'r') as file1:
@@ -80,6 +85,10 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
         #restore epochs number
         with open(os.path.join(train_path, "hyp.txt"), 'r') as file2:
             saved_epochs = int(file2.read())
+
+        #restore lr scheduler state
+        for _ in range(saved_epochs*iter_per_epoch):
+            scheduler.step()
     else:
         wandb_saved_id = wandb.util.generate_id()
         wandb.init(id=wandb_saved_id, resume=True, project="Mobiledet backbone", name = time_now, config=wandb_config)
@@ -95,10 +104,6 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
             file2.close()
         
     print("wandb id:", wandb_saved_id)
-
-    if device == torch.device("cuda"):
-        print("Use wandb watch to collect model info")
-        #wandb.watch(model, log_freq=wandb_log_interval)
 
     time_beginning = time.time()
     losses = {'train': [], "val": []}
@@ -131,9 +136,6 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
                     if train_on_gpu:
                         inputs = inputs.to(device)
                         labels = labels.to(device)
-    
-                    if phase == "train":
-                        optimizer.zero_grad()
 
                     if phase == "train":
                         outputs = model(inputs)
@@ -148,6 +150,7 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
 
                     #calculate 
                     if phase == "train":
+                        optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
                     
@@ -160,9 +163,11 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
                         if iter % wandb_log_interval == 0:
                             wandb.log({"train": {"loss": loss.item(), "accuracy": running_accuracy.item(), "lr": scheduler.get_last_lr()[0]}})
 
-                        #if iter % 100 == 0:
+                        if iter % 100 == 0:
                         #    #torch.save(model.state_dict(), os.path.join(PATH_TO_SAVE, "tmp.pt"))
-                        #    torch.save(model.state_dict(), os.path.join(train_path, "last.pt"))
+                            torch.save(model.state_dict(), os.path.join(train_path, "last.pt"))
+
+                        scheduler.step()
 
                         tepoch.set_postfix(loss=loss.item(), accuracy=running_accuracy.item())
                     if phase == "val":
@@ -192,18 +197,18 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
             accuracy[phase].append(epoch_acc)
 
             if phase == 'train':
-                scheduler.step()
+                #scheduler.step()
                 wandb.log({"train": {"epoch loss": epoch_loss, "epoch accuracy": epoch_acc}})
                 
             if phase == 'val':
                 with open(os.path.join(train_path, "hyp.txt"), 'w') as f:
-                    f.write(epoch+1)
+                    f.write(epoch+1) # +1 because of new epoch - current is done 
                     f.close()
                 correct_top1 = correct_top1.item() / processed_data
                 correct_top5 = correct_top5.item() / processed_data
                 torch.save(model.state_dict(), os.path.join(train_path, "last.pt"))
-                if epoch_acc > best_acc:
-                    best_acc = epoch_acc
+                if correct_top1 > best_acc:
+                    best_acc = correct_top1
                     torch.save(model.state_dict(), os.path.join(train_path, "best.pt"))
                 wandb.log({"val": {"loss": loss, "top1 accuracy": correct_top1, "top5 accuracy": correct_top5}})
 
@@ -217,6 +222,16 @@ def train(model, dataloaders, loss_fn, optimizer, scheduler, num_epochs = 10):
     print("Final top5:", correct_top5, "top1:", correct_top1)
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Mobiledet Backbone training")
+    #parser.add_argument("-r", "--restore", action="store_true", help="flag to restore")
+    parser.add_argument("-id", "--restore_run_id", required=False, help="run id to restore")
+    args, unknown = parser.parse_known_args()
+
+    saved_model = args.restore_run_id
+    if saved_model is not None:
+        load_from_file = True
+
     #Check if GPU is enable
     if torch.cuda.is_available():
         print('CUDA is available!  Training on GPU ...')
@@ -253,25 +268,19 @@ if __name__ == '__main__':
                     for x in ['train', 'val']}
     dataset_sizes = {x: len(imagenet_data[x]) for x in ['train', 'val']}
     class_names = imagenet_data['train'].classes
+    iter_per_epoch = int(len(imagenet_data['train'])/batch_size)
     print(dataset_sizes)
-    train_features, train_labels = next(iter(data_loaders['train']))
 
 
     #Configurate model and hyperparameters
-
     model = mobiledet.MobileDetTPU(net_type="classifier", classes=n_classes)
 
     if load_from_file:
-        #model.load_state_dict(saved_model)
-        model.load_state_dict(torch.load(os.path.join(os.path.join(PATH_TO_SAVE, saved_model), "last.pt"))) #./wandb/run-20221106_155148-2s2e31rf
-
-    #model = torchvision.models.mobilenet_v2(weights=torchvision.models.MobileNet_V2_Weights.IMAGENET1K_V2)
-    #model.classifier = nn.Linear(1280, n_classes)
+        model.load_state_dict(torch.load(os.path.join(os.path.join(PATH_TO_SAVE, saved_model), "last.pt")))
 
     #The optimal learning rate of the MobileNet-V2 model is 1.66 × 10 −3 - ???
-    #optimizer = torch.optim.Adam(model.parameters())
     optimizer = torch.optim.SGD(model.parameters(), init_lr, momentum=init_momentum, weight_decay=init_weight_decay) # CHANGE WANDB!!!
     loss_fn = nn.CrossEntropyLoss()
-    exp_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=int(len(imagenet_data['train'])/batch_size))
+    exp_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=int(EPOCHS*iter_per_epoch))#
 
-    train(model, data_loaders, loss_fn, optimizer, exp_lr_scheduler, EPOCHS)
+    train(model, data_loaders, loss_fn, optimizer, exp_lr_scheduler, iter_per_epoch, EPOCHS)
--- backbone_val.py
+++ backbone_val.py
@@ -15,7 +15,6 @@ import mobiledet
 
 from torchsummary import summary
 
-imagenet_dir = '/media/valdis/NVME/datasets/imagenet/ILSVRC/Data/CLS-LOC'
 batch_size = 128
 workers = 4
 EPOCHS = 1
@@ -78,40 +77,49 @@ def val(model, dataloaders):
     #print("\nEpoch %s/%s" % (epoch+1, num_epochs), "train acc", "{:.4f}".format(accuracy['train'][epoch]), "train loss", "{:.4f}".format(losses['train'][epoch]), 
     #                                                "val acc", "{:.4f}".format(accuracy['val'][epoch]), "val loss", "{:.4f}".format((losses['val'][epoch])))
 
-
-#Check if GPU is enable
-if torch.cuda.is_available():
-    print('CUDA is available!  Training on GPU ...')
-    train_on_gpu = True
-    device = torch.device("cuda")
-else:
-    if torch.backends.mps.is_available():
-        print('GPU on M1 MAC is available!  Training on GPU ...')
+if __name__ == '__main__':
+    #Check if GPU is enable
+    if torch.cuda.is_available():
+        print('CUDA is available!  Training on GPU ...')
         train_on_gpu = True
-        device = torch.device("mps")
+        device = torch.device("cuda")
+        with open("cuda_datapath.txt", 'r') as f:
+            imagenet_dir = f.read()
+            print("cuda imagenet path:", imagenet_dir)
     else:
-        print('CUDA and MPS are not available.  Training on CPU ...')
+        if torch.backends.mps.is_available():
+            print('GPU on M1 MAC is available!  Training on GPU ...')
+            train_on_gpu = True
+            device = torch.device("mps")
+            with open("mps_datapath.txt", 'r') as f:
+                imagenet_dir = f.read()
+                print("mps imagenet path:", imagenet_dir)
+        else:
+            print('CUDA and MPS are not available.  Training on CPU ...')
+            with open("cuda_datapath.txt", 'r') as f:
+                imagenet_dir = f.read()
+                print("cuda imagenet path:", imagenet_dir)
 
-#Loading dataset - like Imagenet, where 1k folders with each classes
-data_transforms = transforms.Compose([
-    transforms.Resize((input_size,input_size)),
-    transforms.ToTensor()
-    ])
-imagenet_data = {x: torchvision.datasets.ImageFolder(os.path.join(imagenet_dir, x), transform=data_transforms)
-                for x in ['train', 'val']}
-data_loaders = {x: torch.utils.data.DataLoader(imagenet_data[x], batch_size=batch_size, shuffle=True, num_workers=workers)
-                for x in ['train', 'val']}
-dataset_sizes = {x: len(imagenet_data[x]) for x in ['train', 'val']}
-class_names = imagenet_data['train'].classes
-print(dataset_sizes['val'])
-#train_features, train_labels = next(iter(data_loaders['train']))
+    #Loading dataset - like Imagenet, where 1k folders with each classes
+    data_transforms = transforms.Compose([
+        transforms.Resize((input_size,input_size)),
+        transforms.ToTensor()
+        ])
+    imagenet_data = {x: torchvision.datasets.ImageFolder(os.path.join(imagenet_dir, x), transform=data_transforms)
+                    for x in ['train', 'val']}
+    data_loaders = {x: torch.utils.data.DataLoader(imagenet_data[x], batch_size=batch_size, shuffle=True, num_workers=workers)
+                    for x in ['train', 'val']}
+    dataset_sizes = {x: len(imagenet_data[x]) for x in ['train', 'val']}
+    class_names = imagenet_data['train'].classes
+    print(dataset_sizes['val'])
+    #train_features, train_labels = next(iter(data_loaders['train']))
 
-print("Model:", MODEL_PATH)
+    print("Model:", MODEL_PATH)
 
-model = mobiledet.MobileDetTPU(net_type="classifier", classes=n_classes)
-model.load_state_dict(torch.load(MODEL_PATH))
+    model = mobiledet.MobileDetTPU(net_type="classifier", classes=n_classes)
+    model.load_state_dict(torch.load(MODEL_PATH))
 
-#mobilenet v2 - Final top5: 0.883 top1: 0.6726
-#model = torchvision.models.mobilenet_v2(weights=torchvision.models.MobileNet_V2_Weights.IMAGENET1K_V2)
+    #mobilenet v2 - Final top5: 0.883 top1: 0.6726
+    #model = torchvision.models.mobilenet_v2(weights=torchvision.models.MobileNet_V2_Weights.IMAGENET1K_V2)
 
-val(model, data_loaders)
+    val(model, data_loaders)
